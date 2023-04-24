38e524c7f4db904cff44156ca69cf3ccc0420c30
Wave FC <malloriepinto89@gmail.com>
2023-04-23 21:42:04 -0700

    draft 2 of JS

--- assets/script.js
+++ assets/script.js
@@ -1,40 +1,85 @@
-var question = documnet.getElementbyID("questions");
-var choices = Array.from(documnet.getbyClassName("Questions-text"));
+const quiz = documnet.getElementbyID("#quiz");
+const choices = Array.from(documnet.querySelectorAll("Questions"));
+const results = document.getElementById('quizFinish');
+const highScores = document.getElementById('highScore');
 
 var currentQuestion = {};
 var acceptingAnswers = true;
 var score = 0;
 var questionCounter = 0
 var availableQuestion = [];
-
 var questions = [
     {
-        question: "",
-        choice1: "",
-        choice2: "",
-        choice3: "",
-        answer: 
-    }
+        question: "Which of the following keywords is used to define a variable in Javascript?",
+        answers: {
+            a: 'var',
+            b: 'let',
+            c: 'Both A and B'
+        },
+        correctAnswer: 'c'
+
+    },
     {
-        question: "",
-        choice1: "",
-        choice2: "",
-        choice3: "",
-        answer: 
-    }
+        question: "Which of the following methods is used to access HTML elements using Javascript?",
+        answers: {
+            a: 'getElementbyId()',
+            b: 'getElementsByClassName()',
+            c: 'Both A and B'
+        },
+        correctAnswer: 'c'
+
+    },
     {
-        question: "",
-        choice1: "",
-        choice2: "",
-        choice3: "",
-        answer: 
-    }
+        question: "Upon encountering empty statements, what does the Javascript Interpreter do?",
+        answers: {
+            a: 'Ignores the statements',
+            b: 'Gives a warning',
+            c: 'Throws an error',
+        },
+        correctAnswer: 'a'
+
+    },
     {
-        question: "",
-        choice1: "",
-        choice2: "",
-        choice3: "",
-        answer: 
+        question: "Which of the following methods can be used to display data in some form using Javascript?",
+        answers: {
+            a: 'console.log()',
+            b: 'window.alert()',
+            c: 'All the Above'
+        },
+        correctAnswer: 'c'
+
     }
-]
+];
+
+var maxQuestions = 4;
+var points = 100;
+
+//start quiz button
+
+//generate quiz
+function startGame() {
+    questionCounter = 0;
+    score = 0;
+    availableQuestion = [...question];
+    getNextQuestions()
+}
+
+//countdown timer
+
+//display questions
+function showQuestions() 
+     
+//show results
+
+function showResults()
+	
+	
+
+//submit initals button
+submitButton.addEventListener('click', ShowResults);
+
+//VHS button
+
+
+startGame();
 
