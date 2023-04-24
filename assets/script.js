836a175e89369e05eff57f952f4336946775d398
Wave FC <malloriepinto89@gmail.com>
2023-04-23 17:04:51 -0700

    draft one of JS

---
+++ assets/script.js
@@ -0,0 +1,40 @@
+var question = documnet.getElementbyID("questions");
+var choices = Array.from(documnet.getbyClassName("Questions-text"));
+
+var currentQuestion = {};
+var acceptingAnswers = true;
+var score = 0;
+var questionCounter = 0
+var availableQuestion = [];
+
+var questions = [
+    {
+        question: "",
+        choice1: "",
+        choice2: "",
+        choice3: "",
+        answer: 
+    }
+    {
+        question: "",
+        choice1: "",
+        choice2: "",
+        choice3: "",
+        answer: 
+    }
+    {
+        question: "",
+        choice1: "",
+        choice2: "",
+        choice3: "",
+        answer: 
+    }
+    {
+        question: "",
+        choice1: "",
+        choice2: "",
+        choice3: "",
+        answer: 
+    }
+]
+
